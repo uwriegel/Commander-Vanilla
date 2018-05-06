865c5c3f3973f40447fcb61c5e3bd2d50edda02c
Uwe Riegel <uriegel@hotmail.de>
2018-05-06 20:13:03 +0200

    Repository created

---
+++ main-typescript/main.ts
@@ -0,0 +1,23 @@
+import { app, BrowserWindow} from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+
+let win
+
+function createWindow () {
+    console.log("Creating window")
+
+    // Erzeugung des Browser Fensters
+    win = new BrowserWindow({width: 800, height: 600})
+
+    // und Laden der index.html der App.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    win.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
