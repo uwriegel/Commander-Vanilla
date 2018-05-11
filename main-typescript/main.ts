33fbdd2b506fe14a9c663a3a8c4a277a4fa07671
Uwe Riegel <uriegel@hotmail.de>
2018-05-11 19:14:50 +0200

    Revert "Revert "STared with main app commander.html, icon, menu, size""
    This reverts commit a124ce73f72617056c8ced09455bae90ac32283b.

--- main-typescript/main.ts
+++ main-typescript/main.ts
@@ -1,33 +1,147 @@
-import { app, BrowserWindow, protocol } from 'electron'
+import { app, BrowserWindow, protocol, Menu } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
+import * as settings from 'electron-settings'
 import * as addon from 'addon'
 
-let win
-
-function createWindow () {
+app.on('ready', () => {
     protocol.registerBufferProtocol('icon', (request, callback) => {
         const ext = decodeURI(request.url).substr(7)
         addon.getIcon(ext, (error, result) => callback(result))
     }, (error) => {}
     )
 
-    console.log("Creating window")
+    const bounds = JSON.parse(settings.get("window-bounds", 
+        JSON.stringify({ 
+            width: 800,
+            height: 600
+        }
+    )) as string)
+
 
     // Erzeugung des Browser Fensters
-    win = new BrowserWindow({width: 800, height: 600})
+    bounds.icon = path.join(__dirname, '../rendering/assets/images/kirk.png')
+    const mainWindow = new BrowserWindow(bounds)
+
+    if (settings.get("isMaximized"))
+        mainWindow.maximize()
 
     // und Laden der index.html der App.
-    win.loadURL(url.format({
+    mainWindow.loadURL(url.format({
+        // Test sites:
         //pathname: path.join(__dirname, '../rendering/scrollbar.html'),
         //pathname: path.join(__dirname, '../rendering/columns.html'),
         //pathname: path.join(__dirname, '../rendering/iconview.html'),
-        pathname: path.join(__dirname, '../rendering/tableview.html'),
+        //pathname: path.join(__dirname, '../rendering/tableview.html'),
+
+        pathname: path.join(__dirname, '../rendering/commander.html'),
+
         protocol: 'file:',
         slashes: true
     }))
 
-    //win.webContents.openDevTools()
-}
+    mainWindow.on('close', () => {
+        if (!mainWindow.isMaximized()) {
+            const bounds = mainWindow.getBounds()
+            settings.set("window-bounds", JSON.stringify(bounds))
+        }
+    })
 
-app.on('ready', createWindow)
+    mainWindow.on('maximize', () => {
+        const bounds = mainWindow.getBounds()
+        settings.set("window-bounds", JSON.stringify(bounds))
+        settings.set("isMaximized", true)
+    })
+
+    mainWindow.on('unmaximize', () => {
+        settings.set("isMaximized", false)
+    })
+
+    const menu = Menu.buildFromTemplate([
+        {
+            label: '&Datei',
+            submenu: [{
+                label: '&Umbenennen',
+                accelerator: "F2"
+            },
+            {
+                type: 'separator'
+            },            
+            {
+                label: '&Kopieren',
+                accelerator: "F5"
+            },
+            {
+                label: '&Verschieben',
+                accelerator: "F6"
+            },
+            {
+                label: '&Löschen',
+                accelerator: "F8"
+            },
+            {
+                type: 'separator'
+            },            
+            {
+                label: '&Eigenschaften',
+                accelerator: "Alt+Enter"
+            },
+            {
+                label: '&Beenden',
+                accelerator: 'Alt+F4',
+                role: "quit"
+            }
+        ]},
+        {
+            label: '&Navigation',
+            submenu: [{
+                label: '&Erstes Element',
+                accelerator: 'Home'
+            },
+            {
+                label: '&Favoriten',
+                accelerator: 'F1'
+            }
+        ]},
+        {
+            label: '&Selection',
+            submenu: [{
+                label: '&Alles'
+            },
+            {
+                label: '&Nichts'
+            }
+        ]},
+        {
+            label: '&Ansicht',
+            submenu: [{
+                label: '&Versteckte Dateien',
+                accelerator: "Ctrl+H",
+                type: "checkbox"
+            },
+            {
+                label: '&Dunkles Thema',
+                type: "checkbox"
+            },
+            {
+                type: 'separator'
+            },            
+            {
+                label: '&Vollbild',
+                click: () => mainWindow.setFullScreen(!mainWindow.isFullScreen()),
+                accelerator: "F11"
+            },
+            {
+                type: 'separator'
+            },            
+            {
+                label: '&Entwicklerwerkzeuge',
+                click: () => mainWindow.webContents.openDevTools(),
+                accelerator: "F12"
+            }
+        ]}
+    ])
+    
+    Menu.setApplicationMenu(menu)    
+})
+
