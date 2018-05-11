a124ce73f72617056c8ced09455bae90ac32283b
Uwe Riegel <uriegel@hotmail.de>
2018-05-11 19:14:44 +0200

    Revert "STared with main app commander.html, icon, menu, size"
    This reverts commit 3f269f76c06ee55d10c73e264f7fdcc7cc10f0ee.

--- main-typescript/main.ts
+++ main-typescript/main.ts
@@ -1,147 +1,33 @@
-import { app, BrowserWindow, protocol, Menu } from 'electron'
+import { app, BrowserWindow, protocol } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
-import * as settings from 'electron-settings'
 import * as addon from 'addon'
 
-app.on('ready', () => {
+let win
+
+function createWindow () {
     protocol.registerBufferProtocol('icon', (request, callback) => {
         const ext = decodeURI(request.url).substr(7)
         addon.getIcon(ext, (error, result) => callback(result))
     }, (error) => {}
     )
 
-    const bounds = JSON.parse(settings.get("window-bounds", 
-        JSON.stringify({ 
-            width: 800,
-            height: 600
-        }
-    )) as string)
-
+    console.log("Creating window")
 
     // Erzeugung des Browser Fensters
-    bounds.icon = path.join(__dirname, '../rendering/assets/images/kirk.png')
-    const mainWindow = new BrowserWindow(bounds)
-
-    if (settings.get("isMaximized"))
-        mainWindow.maximize()
+    win = new BrowserWindow({width: 800, height: 600})
 
     // und Laden der index.html der App.
-    mainWindow.loadURL(url.format({
-        // Test sites:
+    win.loadURL(url.format({
         //pathname: path.join(__dirname, '../rendering/scrollbar.html'),
         //pathname: path.join(__dirname, '../rendering/columns.html'),
         //pathname: path.join(__dirname, '../rendering/iconview.html'),
-        //pathname: path.join(__dirname, '../rendering/tableview.html'),
-
-        pathname: path.join(__dirname, '../rendering/commander.html'),
-
+        pathname: path.join(__dirname, '../rendering/tableview.html'),
         protocol: 'file:',
         slashes: true
     }))
 
-    mainWindow.on('close', () => {
-        if (!mainWindow.isMaximized()) {
-            const bounds = mainWindow.getBounds()
-            settings.set("window-bounds", JSON.stringify(bounds))
-        }
-    })
+    //win.webContents.openDevTools()
+}
 
-    mainWindow.on('maximize', () => {
-        const bounds = mainWindow.getBounds()
-        settings.set("window-bounds", JSON.stringify(bounds))
-        settings.set("isMaximized", true)
-    })
-
-    mainWindow.on('unmaximize', () => {
-        settings.set("isMaximized", false)
-    })
-
-    const menu = Menu.buildFromTemplate([
-        {
-            label: '&Datei',
-            submenu: [{
-                label: '&Umbenennen',
-                accelerator: "F2"
-            },
-            {
-                type: 'separator'
-            },            
-            {
-                label: '&Kopieren',
-                accelerator: "F5"
-            },
-            {
-                label: '&Verschieben',
-                accelerator: "F6"
-            },
-            {
-                label: '&Löschen',
-                accelerator: "F8"
-            },
-            {
-                type: 'separator'
-            },            
-            {
-                label: '&Eigenschaften',
-                accelerator: "Alt+Enter"
-            },
-            {
-                label: '&Beenden',
-                accelerator: 'Alt+F4',
-                role: "quit"
-            }
-        ]},
-        {
-            label: '&Navigation',
-            submenu: [{
-                label: '&Erstes Element',
-                accelerator: 'Home'
-            },
-            {
-                label: '&Favoriten',
-                accelerator: 'F1'
-            }
-        ]},
-        {
-            label: '&Selection',
-            submenu: [{
-                label: '&Alles'
-            },
-            {
-                label: '&Nichts'
-            }
-        ]},
-        {
-            label: '&Ansicht',
-            submenu: [{
-                label: '&Versteckte Dateien',
-                accelerator: "Ctrl+H",
-                type: "checkbox"
-            },
-            {
-                label: '&Dunkles Thema',
-                type: "checkbox"
-            },
-            {
-                type: 'separator'
-            },            
-            {
-                label: '&Vollbild',
-                click: () => mainWindow.setFullScreen(!mainWindow.isFullScreen()),
-                accelerator: "F11"
-            },
-            {
-                type: 'separator'
-            },            
-            {
-                label: '&Entwicklerwerkzeuge',
-                click: () => mainWindow.webContents.openDevTools(),
-                accelerator: "F12"
-            }
-        ]}
-    ])
-    
-    Menu.setApplicationMenu(menu)    
-})
-
+app.on('ready', createWindow)
