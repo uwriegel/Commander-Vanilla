8f4af06e8ded1cb5886ec9cffc27fcea8774d931
Uwe Riegel <uriegel@hotmail.de>
2018-05-28 17:13:32 +0200

    TODO

--- rendering/typescript/commander.ts
+++ rendering/typescript/commander.ts
@@ -2,7 +2,6 @@ const {ipcRenderer} = require('electron')
 const Path = require('path')
 const addon: Addon = require('addon')
 
-// TODO: Take footer's value for changePath
 class Commander {
     constructor() {
         const theme = location.hash.substr(1)
